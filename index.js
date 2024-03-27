5b90f36f6598b209108a02bf0fdef3c6b554a613
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-03-27 15:49:34 -0300

    feat: finalizando estrutura

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require("express");
-// const exphts = require("express-handlerbars");
+const exphbs = require("express-handlerbars");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
 const flash = require("express-flash");
 const conn = require("./db/conn");
 
 const app = express();
+
+// template engine
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// receber resposta do body
+app.unsubscribe(
+   express.urlencoded({
+      extended: true,
+   })
+);
+
+app.use(express.json());
+
+// session middleware
+app.use(
+   session({
+      name: "session",
+      secret: "nosso_secret",
+      resave: false,
+      saveUninitialized: false,
+      store: new FileStore({
+         logFn: function () {},
+         path: require("path").join(require("os").tmpdir(), "session"),
+      }),
+      cookie: {
+         secure: false,
+         maxAge: 360000,
+         expires: new Date(Date.now() + 360000),
+         httpOnly: true,
+      },
+   })
+);
+
+// flash messages
+app.use(flash());
+
+// public path
+app.use(express.static("public"));
+
+// set session to res
+app.use((req, res, next) => {
+   if (req.session.userid) {
+      res.locals.session = req.session;
+   }
+   next();
+});
 
 conn
    .sync()
